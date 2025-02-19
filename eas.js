2c6b2c9c482c7734c8303de194f17845a64a813e
June <yoo.junehyuk@gmail.com>
2025-02-18 22:41:21 -0600

    Added drawing onto the tiles.
    Implemented tile drawing through event listeners in eas.js. Updated UI as well in style.css.

--- eas.js
+++ eas.js
@@ -6,10 +6,32 @@ let tileSize = containerSize / length;
 let container = document.querySelector(".container");
 container.setAttribute("style", `width: ${containerSize}px; height: ${containerSize}px;`);
 
+// Add tile divs into the container div as children
 for (let i = 0; i < length * length; i++) {
     let div = document.createElement("div");
     div.classList.add("tile");
     div.setAttribute("style", `width: ${tileSize}px; height: ${tileSize}px;`);
     container.appendChild(div);
-
 }
+
+let isDrawing = false;
+
+// Function to color in cells on mousedown and mouseover
+function colorTile(e) {
+    if (!isDrawing) return;
+    e.target.style.backgroundColor = "black";
+}
+// Add mousedown and mouseover event listeners for each time
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach(tile => {
+    tile.addEventListener("mousedown", (e) => {
+        isDrawing = true;
+        colorTile(e);
+    });
+    tile.addEventListener("mouseover", colorTile);
+});
+
+// Check for mouseup
+document.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
